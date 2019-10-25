d97161530a737e7211e7f5454842dd4a52aeb087
Matheus Ribeiro <matheus.gomes94@etec.sp.gov.br>
2019-10-25 14:33:14 -0300

    updating the src code

---
+++ routes.js
@@ -0,0 +1,67 @@
+const express=require("express")
+
+const ObjectId = require('mongodb').ObjectID
+
+const app=express.Router();
+
+app.route('/') //setado a rota, e abaixo as ações a serem tomadas dentro desta rota
+.get(function(req, res) {
+  const cursor = db.collection('data').find()
+  res.render('index.ejs')
+})
+
+.post((req, res) => {
+  db.collection('data').insertOne(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('Salvo no Banco de Dados')
+    res.redirect('/show')
+  })
+})
+
+app.route('/show')
+.get((req, res) => {
+  db.collection('data').find().toArray((err, results) => {
+    if (err) return console.log(err)
+    res.render('show.ejs', { data: results })
+  })
+})
+
+app.route('/edit/:id')
+.get((req, res) => {
+  var id = req.params.id
+
+  db.collection('data').find(ObjectId(id)).toArray((err, result) => {
+    if (err) return res.send(err)
+    res.render('edit.ejs', { data: result })
+  })
+})
+.post((req, res) => {
+  var id = req.params.id
+  var name = req.body.name
+  var surname = req.body.surname
+
+  db.collection('data').updateOne({_id: ObjectId(id)}, {
+    $set: {
+      name: name,
+      surname: surname
+    }
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.redirect('/show')
+    console.log('Atualizado no Banco de Dados')
+  })
+})
+
+app.route('/delete/:id')
+.get((req, res) => {
+  var id = req.params.id
+
+  db.collection('data').deleteOne({_id: ObjectId(id)}, (err, result) => {
+    if (err) return res.send(500, err)
+    console.log('Deletado do Banco de Dados!')
+    res.redirect('/show')
+  })
+})
+
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const ObjectId = require('mongodb').ObjectID
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb://169.254.206.253:27017";
+
+const rotas=require("./routes")
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect(uri,{useNewUrlParser:true}, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('crud-nodejs') // coloque o nome do seu DB
+
+  app.listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
+})
+
+//Tipo de template engine
+app.set('view engine', 'ejs')
+app.use(express.static('./public'));
+app.use(rotas)
